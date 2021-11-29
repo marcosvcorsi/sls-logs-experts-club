d39e7de6f91ecf9e20b2491a26c92f14c3a92431
Marcos Corsi <marcosvcorsi@gmail.com>
2021-11-28 23:20:16 -0300

    feat: add audit logs persistence

--- src/consumer.js
+++ src/consumer.js
@@ -1,5 +1,24 @@
+const AWSXray = require('aws-xray-sdk-core')
+const AWS = AWSXray.captureAWS(require('aws-sdk'))
+
+const getUuid = require('uuid-by-string')
+
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
 const recordDeletedAccountInfo = event => {
-  console.log(event)
+  const [{ body }] = event.Records
+
+  const { email, timestamp } = JSON.parse(body)
+
+  console.log({ email, timestamp })
+
+  return documentClient.put({
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      userId: getUuid(email, 'rocket'),
+      deletedAt: timestamp,
+    },
+  }).promise()
 }
 
 module.exports = {

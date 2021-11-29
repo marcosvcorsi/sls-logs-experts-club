63262ca8efe7f7c3f00c2acaad4d635683fface7
Marcos Corsi <marcosvcorsi@gmail.com>
2021-11-28 22:31:54 -0300

    feat: add delete account publisher

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
+const AWSXray = require('aws-xray-sdk-core')
+const AWS = AWSXray.captureAWS(require('aws-sdk'))
+
 const deleteAccount = async event => {
-  console.log(event)
+  const sns = new AWS.SNS()
+
+  const deleteAccountEvent = {
+    ...event,
+    timestamp: Math.floor(new Date().getTime() / 1000),
+  }
+
+  console.log(deleteAccountEvent)
+
+  return sns.publish({
+    Message: JSON.stringify(deleteAccountEvent),
+    TopicArn: process.env.SNS_TOPIC,
+  }).promise()
 }
 
 module.exports = {
